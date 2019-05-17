71776e41ba4cf31a1d0bf770fd5ff4b0e629890a
BORBER <borber.drum@gmail.com>
2019-05-17 09:18:20 +0800

    以 字符/字节 读取 文件

--- forfile/DoFile.java
+++ forfile/DoFile.java
@@ -1,7 +1,6 @@
 package forfile;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 /**
  * @author BORBER
@@ -17,13 +16,18 @@ public class DoFile {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }else {
+            System.out.println("File is already exists,so I can't creat it.");
         }
         return false;
     }
+
     public static boolean createDirectory(String directory){
         File file = new File(directory);
         if(!file.exists()){
             return file.mkdir();
+        }else {
+            System.out.println("Directory is already exists, so I can't creat it.");
         }
         return false;
     }
@@ -32,6 +36,8 @@ public class DoFile {
         File file = new File(filepath);
         if(file.exists() && file.isFile()){
             return file.delete();
+        }else {
+            System.out.println("File is not exists or it is not a file, so I can't delete it.");
         }
         return false;
     }
@@ -39,6 +45,7 @@ public class DoFile {
     public static boolean deleteDirectory(String filepath){
         File file = new File(filepath);
         if(!file.exists()){
+            System.out.println("File if not exists so I can't delete it.");
             return false;
         }
         if(file.isFile()){
@@ -53,4 +60,47 @@ public class DoFile {
         return false;
     }
 
+    public static String readFileByBytes(String filepath){
+        File file = new File(filepath);
+        if(!file.exists() || !file.isFile()){
+            System.out.println("File is not exists or it is not a file,so I can't read it.");
+            return null;
+        }
+        StringBuffer content = new StringBuffer();
+        try {
+            byte[] tmp = new byte[1024];
+            FileInputStream fileInputStream = new FileInputStream(file);
+            while (fileInputStream.read(tmp) != -1){
+                content.append(new String(tmp));
+                tmp = new byte[1024];
+            }
+            fileInputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return content.toString();
+    }
+
+    public static String readFileByChars(String filepath){
+        File file = new File(filepath);
+        if(!file.exists() || !file.isFile()){
+            System.out.println("File is not exists or it is not a file,so I can't read it.");
+            return null;
+        }
+        StringBuffer content = new StringBuffer();
+        try {
+            char[] tmp = new char[1024];
+            FileInputStream fileInputStream = new FileInputStream(file);
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream,"GBK");
+            while (inputStreamReader.read(tmp) != -1){
+                content.append(new String(tmp));
+                tmp = new char[1024];
+            }
+            fileInputStream.close();
+            inputStreamReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return content.toString();
+    }
 }
