48a2583117445c926b63dc72732a3b6e1de8c875
DRUM <borber.drum@gmail.com>
2019-09-11 23:57:04 +0800

    改进 ArrayList 实现任意类型的 ArrayList

--- forExam/UnivariatePolynomial.java
+++ forExam/UnivariatePolynomial.java
@@ -1,4 +1,35 @@
-package forList;
+package forExam;
+
+import java.util.Scanner;
+
+/**
+ * @program: Univariate_polynomial
+ * @description: 设计函数分别求两个一元多项式的乘积与和。
+ * 输入格式:
+ * 输入分2行，每行分别先给出多项式非零项的个数，再以指数递降方式输入一个多项式非零项系数和指数（绝对值均为不超过1000的整数）。数字间以空格分隔。
+ *
+ * 输出格式:
+ * 输出分2行，分别以指数递降方式输出乘积多项式以及和多项式非零项的系数和指数。数字间以空格分隔，但结尾不能有多余空格。零多项式应输出0 0。
+ *
+ * 输入样例:
+ * 4 3 4 -5 2  6 1  -2 0
+ * 3 5 20  -7 4  3 1
+ * 输出样例:
+ * 15 24 -25 22 30 21 -10 20 -21 8 35 6 -33 5 14 4 -15 3 18 2 -6 1
+ * 5 20 -4 4 -5 2 9 1 -2 0
+ * @author: BORBER
+ * @create: 2019-09-11 23:03
+ * @version: 1.0
+ **/
+public class UnivariatePolynomial {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        MyArrayList list = new MyArrayList();
+        list.add(1,2);
+        System.out.println(list.get(0).modulus);
+        System.out.println(list.get(0).index);
+    }
+}
 
 /**
  * @program: MyArrayList
@@ -13,7 +44,7 @@ class MyArrayList {
 
 
 
-    class Doub{
+    static class Doub{
         int modulus;
         int index;
         Doub(int modulus,int index){
@@ -110,7 +141,7 @@ class MyArrayList {
             throw new ArrayIndexOutOfBoundsException();
         }
         Doub old = theItems[idx];
-        theItems[idx] = new Doub(a,b);
+        theItems[idx] = new Doub(a, b);
         return old;
     }
 
@@ -152,7 +183,7 @@ class MyArrayList {
         if (size() - idx >= 0) {
             System.arraycopy(theItems, idx, theItems, idx + 1, size() - idx);
         }
-        theItems[idx] = new Doub(a,b);
+        theItems[idx] = new Doub(a, b);
         ++theSize;
     }
 
